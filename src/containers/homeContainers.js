51e714efcb00b3e569548d9b61f2616fd6ce1cf1
Krunal <krunal.chavda@anblicks.com>
2023-12-27 15:43:46 +0530

    add to cart design done

---
+++ src/containers/homeContainers.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "../components/Home";
+
+function HomeContainers() {
+  return (
+    <div>
+      <Home />
+      <h1>HomeContainers Component</h1>
+    </div>
+  );
+}
+
+export default HomeContainers;
